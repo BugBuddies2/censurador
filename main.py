16b9bb73fb667a2ec462f84192ef0682055a4809
Schlieperricardo <ricardoschlieper@yahoo.com.br>
2025-04-14 20:14:20 -0300

    panana

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from filter import filtrar_texto
 
+
+with open(text_file, "r") as file:
+    texto = file.read()
+
 #teste rapido
-texto = "esse bobão e fanfarrão fica se achando o bobinho da turma"
-palavras_ruins = ["bobão", "fanfarrão", "bobinho"]
+
+palavras_ruins = ["bobão", "fanfarrão", "bobinho", "panaca", "banana", "tanso", "lesado"]
 
 print("normal:")
 print(filtrar_texto(texto, palavras_ruins))
