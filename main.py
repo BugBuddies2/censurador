f50813e877b3fd64605692372473f402d1fb4d90
ManfredHair <manfredhair@gmail.com>
2025-04-14 20:29:04 -0300

    output vai para arquivo

--- main.py
+++ main.py
@@ -8,8 +8,19 @@ with open("texto.txt", "r") as file:
 with open("lista.txt", "r") as file:
     palavras_ruins = [linha.strip() for linha in file]
 
+# filtra o texto
+texto_filtrado = filtrar_texto(texto, palavras_ruins)
+texto_filtrado_censurado = filtrar_texto(texto, palavras_ruins, substituir="[CENSURADO]")
+
+# mostra na tela
 print("normal:")
-print(filtrar_texto(texto, palavras_ruins))
+print(texto_filtrado)
 
 print("\ncom [CENSURADO]:")
-print(filtrar_texto(texto, palavras_ruins, substituir="[CENSURADO]")) 
+print(texto_filtrado_censurado)
+
+# salva em arquivo
+with open("censurado.txt", "w") as file:
+    file.write(texto_filtrado)
+
+print("\nArquivo 'censurado.txt' foi criado com o texto filtrado!") 
